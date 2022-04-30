6d38f078adb000f4291b74e19a0d6b38208df0a6
Abyou <emandaabyou@gmail.com>
2022-04-29 23:57:13 -0500

    Build HTML template, generate HTML file and modularize the code

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const [name, github] = profileDataArgs;
 
 fs.writeFile("./index.html", generatePage(name, github), (err) => {
   if (err) throw new Error(err);
-  console.log(err);
 
-  console.log("portfolio complete! Check out index.html to see the output!");
+  console.log("Portfolio complete! Check out index.html to see the output!");
 });
